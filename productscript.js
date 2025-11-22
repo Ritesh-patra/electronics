144b3e7064015a39790536402e9f741791fb3442
Ritesh-patra <patrasagarika654@gmail.com>
2025-11-23 01:28:23 +0530

    added new video

--- productscript.js
+++ productscript.js
@@ -1,11 +1,23 @@
-    const watchDemoBtn = document.getElementById("watchDemoBtn");
-    const videoPopup = document.getElementById("videoPopup");
-    const closePopup = document.getElementById("closePopup");
+const watchDemoBtn = document.getElementById("watchDemoBtn");
+const videoPopup = document.getElementById("videoPopup");
+const closePopup = document.getElementById("closePopup");
+const popupVideo = document.getElementById("popupVideo");
 
-    watchDemoBtn.addEventListener("click", () => {
-        videoPopup.classList.remove("hidden");
-    });
+watchDemoBtn.addEventListener("click", () => {
+    // popup show
+    videoPopup.classList.remove("hidden");
 
-    closePopup.addEventListener("click", () => {
-        videoPopup.classList.add("hidden");
-    });
+    // play from start with sound
+    popupVideo.currentTime = 0;
+    popupVideo.muted = false;
+    popupVideo.play();
+});
+
+closePopup.addEventListener("click", () => {
+    // popup hide
+    videoPopup.classList.add("hidden");
+
+    // stop video completely (sound off)
+    popupVideo.pause();
+    popupVideo.currentTime = 0;
+});
